fa2be8595002e05a7b12a2f99906a81b71cca9b7
Juliano Pimentel <juliano_pimentel@live.com>
2021-08-12 19:30:39 -0300

    correcao no menu nao fixado

--- resources/views/dashboard/shared/header.blade.php
+++ resources/views/dashboard/shared/header.blade.php
@@ -1,7 +1,7 @@
 
       
     <div class="c-wrapper">
-      <header class="c-header c-header-light c-header-fixed c-header-with-subheader">
+      <header class="c-header c-header-light c-header-with-subheader">
         <button class="c-header-toggler c-class-toggler d-lg-none mr-auto" type="button" data-target="#sidebar" data-class="c-sidebar-show"><span class="c-header-toggler-icon"></span></button>
         <?php
             use App\MenuBuilder\FreelyPositionedMenus;
