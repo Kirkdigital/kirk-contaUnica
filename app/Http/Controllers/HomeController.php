6a99d2b5bd2ba236e3909f11e9fe27b49d064e6c
Juliano Pimentel <juliano_pimentel@live.com>
2021-08-22 17:11:36 -0300

    melhoria na home
    ajustar o gráfico

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,16 +11,10 @@ use App\Models\Event;
 use App\Models\Historic;
 use App\Models\Config_meta;
 use App\Models\Config_social;
-use App\Models\Group;
 use App\Models\People_Groups;
 use App\Models\People_Precadastro;
 use App\Models\Roles;
-use App\Models\User;
-use Illuminate\Support\Facades\Config;
-use Illuminate\Support\Facades\DB;
 use Overtrue\LaravelLike\Traits\Likeable;
-use Doctrine\DBAL\Events;
-use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -52,6 +46,8 @@ class HomeController extends Controller
 
         //pegar informações complementares 
         $meta = Config_meta::orderBy('id', 'desc')->first();
+        $roles = Roles::orderBy('id', 'desc')->first();
+
 
         if($meta === null)
         {
@@ -83,9 +79,30 @@ class HomeController extends Controller
         //numero de pessoas ativas e no ano atual
         $precadastro = People_Precadastro::where('status_id', '21')->count();;
 
+        //eventos para listar depois
         $eventos = Event::whereYear('created_at', date('Y'))->count();
+        //recado para listar depois
         $message = Notes::whereYear('created_at', date('Y'))->count();
+        //links das redes sociais
         $social = Config_social::find('1')->first();
+        //dash de status
+        $people = People::all();        
+        $peopleativo = $people->count();
+        $totalvisitante = $people->where('is_visitor', true)->count();
+        $totalbatismo = $people->where('is_baptism', true)->count();
+        $totalconversao = $people->where('is_conversion', true)->count();
+
+        //financeiro recente
+        $date = date('Y-m');
+        $dizimoatual = Historic::where('tipo', '9')->where('date','like', "%$date%")->sum('amount');
+        $ofertaatual = Historic::where('tipo', '10')->where('date','like',"%$date%")->sum('amount');
+        $doacaoatual = Historic::where('tipo', '11')->where('date','like', "%$date%")->sum('amount');
+        $despesaatual = Historic::where('tipo', '12')->where('date','like', "%$date%")->sum('amount');
+
+        $porcentage_dizimo = $this->porcentagem_nx($dizimoatual, $meta->fin_dizimo_mes); // 20
+        $porcentage_oferta = $this->porcentagem_nx($ofertaatual, $meta->fin_oferta_mes); 
+        $porcentage_doacao = $this->porcentagem_nx($doacaoatual, $meta->fin_acao_mes); // 20
+        $porcentage_despesa = $this->porcentagem_nx($despesaatual, $meta->fin_despesa_mes); 
 
         $tenant = $request->session()->get('schema');
         foreach ($tenant as $element) {
@@ -108,8 +125,29 @@ class HomeController extends Controller
                     'message',
                     'social',
                     'a',
-                    'user'
+                    'user',
+                    'roles',
+                    'peopleativo',
+                    'totalvisitante',
+                    'totalbatismo',
+                    'totalconversao',
+                    'dizimoatual',
+                    'ofertaatual',
+                    'doacaoatual',
+                    'despesaatual',
+                    'porcentage_dizimo',
+                    'porcentage_oferta',
+                    'porcentage_doacao',
+                    'porcentage_despesa',
                 ), ['groups' => $groups]);
                 
     }
+        //calcular porcentagem individual x total
+        function porcentagem_nx ( $parcial, $total ) {
+            if($total == 0) {
+                $ratio = 0;
+            } else {
+                return ( $parcial * 100 ) / $total;
+            } 
+        }
 }
